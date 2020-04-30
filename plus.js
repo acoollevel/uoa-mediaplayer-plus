eae439c961a0085fca5cc23d6c741b827ee8b2bc
acoollevel <levi.beintken@gmail.com>
2020-04-30 12:38:08 +1200

    Added play/pause button

--- plus.js
+++ plus.js
@@ -10,25 +10,40 @@ document.arrive(".shaka-volume-bar-container", function() {
 
         vid = document.getElementById("video");
         vid.addEventListener('play', function() {
-            console.log(vid.playbackRate)
-            vid.playbackRate = intended_speed;
-            console.log(vid.playbackRate)
+            vid.playbackRate = intended_speed; // make sure playback speed is still correct
+            document.getElementById("mpp-play").innerHTML = "pause" // update play icon
+        })
+        vid.addEventListener('pause', function() {
+            document.getElementById("mpp-play").innerHTML = "play_arrow" // update play icon
         })
         controls = document.getElementsByClassName("shaka-controls-container")[0]
         vol_slider = document.getElementsByClassName("shaka-volume-bar-container")[0]
 
+        // download button
         download_button = "<button class='material-icons' id='mpp-download' aria-label='Download' title='Download'>get_app</button>"
-        vol_slider.insertAdjacentHTML("afterend", download_button)
+        vol_slider.insertAdjacentHTML("afterend", download_button);
         document.getElementById("mpp-download").addEventListener('click', function() {
             url_string = window.location.href;
             url_string = url_string.replace('.preview', '.mp4');
             window.open(url_string,'_blank');
         });
 
+        // keep intended speed
         speed_changer = document.getElementsByClassName("shaka-playback-rates")[0]
         speed_changer.addEventListener('click', function() {
             intended_speed = vid.playbackRate;
         })
+
+        // play/pause button
+        play_button = "<button class='material-icons' id='mpp-play' aria-label='Play/Pause' title='Play/Pause'>play_arrow</button>";
+        document.getElementsByClassName("shaka-current-time")[0].insertAdjacentHTML("beforebegin", play_button);
+        document.getElementById("mpp-play").addEventListener('click', function() {
+            if (vid.paused) {
+                vid.play();
+            } else {
+                vid.pause();
+            }
+        });
     }
 });
 
