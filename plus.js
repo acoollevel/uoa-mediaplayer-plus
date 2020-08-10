a879aa18af0e0ebd2b938da5d3766f8bce1381dd
LRTNZ <liam@aacl.co.nz>
2020-08-11 01:20:40 +1200

    style: Formatting of the shortcuts keycheck logic

--- plus.js
+++ plus.js
@@ -214,100 +214,100 @@ document.arrive(".shaka-volume-bar-container", function() {
 
 // Keybindings
 if(player_id == "mediaplayer"){
-document.addEventListener('keydown', function(event) {
+    document.addEventListener('keydown', function(event) {
 
-    // prevent unexpected browser behaviour
-    event.preventDefault();
-    document.activeElement.blur();
+        // prevent unexpected browser behaviour
+        event.preventDefault();
+        document.activeElement.blur();
 
-    try {
-        clearTimeout(timeout);
-    } catch {}
-    controls.setAttribute("shown", "true");
-    timeout = setTimeout(function(){ controls.removeAttribute("shown"); }, 1000);
+        try {
+            clearTimeout(timeout);
+        } catch {}
+        controls.setAttribute("shown", "true");
+        timeout = setTimeout(function(){ controls.removeAttribute("shown"); }, 1000);
 
-    // Pause with spacebar, 'k'
-    if(event.keyCode == 32 || event.keyCode == 75) {
-        if (vid.paused) {
-            vid.play();
-            show_popup("play_arrow", "Play");
-        } else {
-            vid.pause();
-            show_popup("pause", "Pause");
+        // Pause with spacebar, 'k'
+        if(event.keyCode == 32 || event.keyCode == 75) {
+            if (vid.paused) {
+                vid.play();
+                show_popup("play_arrow", "Play");
+            } else {
+                vid.pause();
+                show_popup("pause", "Pause");
+            }
         }
-    }
 
-    // Go fullscreen with 'f'
-    if(event.keyCode == 70) {
-        document.getElementsByClassName("shaka-fullscreen-button")[0].click();
-    }
-
-    // Seek forwards with '➡', 'l'
-    if(event.keyCode == 39 || event.keyCode == 76) {
-        vid.currentTime = vid.currentTime + 5;
-        show_popup("skip_next", "Seek");
-    }
-
-    // Seek backwards with '⬅', 'j'
-    if(event.keyCode == 37 || event.keyCode == 74) {
-        vid.currentTime = vid.currentTime - 5;
-        show_popup("skip_previous", "Seek");
-    }
-
-    // Volume up with '⬆'
-    if(event.keyCode == 38) {
-        vid.volume = vid.volume + 0.05;
-        if (vid.volume > 0.95) {
-            vid.volume = 1;
+        // Go fullscreen with 'f'
+        if(event.keyCode == 70) {
+            document.getElementsByClassName("shaka-fullscreen-button")[0].click();
         }
-        show_popup("volume_up", Math.round(vid.volume*100));
-    }
 
-    // Volume up with '⬇'
-    if(event.keyCode == 40) {
-        vid.volume = vid.volume - 0.05;
-        if (vid.volume < 0.05) {
-            vid.volume = 0;
+        // Seek forwards with '➡', 'l'
+        if(event.keyCode == 39 || event.keyCode == 76) {
+            vid.currentTime = vid.currentTime + 5;
+            show_popup("skip_next", "Seek");
         }
-        show_popup("volume_down", Math.round(vid.volume*100));
-    }
 
-    // Increase speed with '.'
-    if(event.keyCode == 190) {
-        vid.playbackRate = vid.playbackRate + 0.25;
-        if (vid.playbackRate > 3) {
-            vid.playbackRate = 3;
+        // Seek backwards with '⬅', 'j'
+        if(event.keyCode == 37 || event.keyCode == 74) {
+            vid.currentTime = vid.currentTime - 5;
+            show_popup("skip_previous", "Seek");
         }
-        intended_speed = vid.playbackRate;
-        show_popup("fast_forward", vid.playbackRate + "x");
-    }
 
-    // Decrease speed with ','
-    if(event.keyCode == 188) {
-        vid.playbackRate = vid.playbackRate - 0.25;
-        if (vid.playbackRate < 0.25) {
-            vid.playbackRate = 0.25;
+        // Volume up with '⬆'
+        if(event.keyCode == 38) {
+            vid.volume = vid.volume + 0.05;
+            if (vid.volume > 0.95) {
+                vid.volume = 1;
+            }
+            show_popup("volume_up", Math.round(vid.volume*100));
         }
-        intended_speed = vid.playbackRate;
-        show_popup("fast_rewind", vid.playbackRate + "x");
-    }
 
-    // Reset speed with '/'
-    if(event.keyCode == 191) {
-        vid.playbackRate = 1;
-        intended_speed = vid.playbackRate;
-        show_popup("speed", "1x");
-    }
-
-    // Mute/unmute with 'm'
-    if(event.keyCode == 77) {
-        if (vid.muted) {
-            vid.muted = false;
-            show_popup("volume_up", "Unmuted");
-        } else {
-            vid.muted = true;
-            show_popup("volume_off", "Muted");
+        // Volume up with '⬇'
+        if(event.keyCode == 40) {
+            vid.volume = vid.volume - 0.05;
+            if (vid.volume < 0.05) {
+                vid.volume = 0;
+            }
+            show_popup("volume_down", Math.round(vid.volume*100));
         }
-    }
-});
+
+        // Increase speed with '.'
+        if(event.keyCode == 190) {
+            vid.playbackRate = vid.playbackRate + 0.25;
+            if (vid.playbackRate > 3) {
+                vid.playbackRate = 3;
+            }
+            intended_speed = vid.playbackRate;
+            show_popup("fast_forward", vid.playbackRate + "x");
+        }
+
+        // Decrease speed with ','
+        if(event.keyCode == 188) {
+            vid.playbackRate = vid.playbackRate - 0.25;
+            if (vid.playbackRate < 0.25) {
+                vid.playbackRate = 0.25;
+            }
+            intended_speed = vid.playbackRate;
+            show_popup("fast_rewind", vid.playbackRate + "x");
+        }
+
+        // Reset speed with '/'
+        if(event.keyCode == 191) {
+            vid.playbackRate = 1;
+            intended_speed = vid.playbackRate;
+            show_popup("speed", "1x");
+        }
+
+        // Mute/unmute with 'm'
+        if(event.keyCode == 77) {
+            if (vid.muted) {
+                vid.muted = false;
+                show_popup("volume_up", "Unmuted");
+            } else {
+                vid.muted = true;
+                show_popup("volume_off", "Muted");
+            }
+        }
+    });
 }
