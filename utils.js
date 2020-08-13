30b8e44c11ae41ee880a970c5423a73c563c9891
LRTNZ <liam@aacl.co.nz>
2020-08-13 19:31:27 +1200

    feat: Added method to automatically select the default resolution
    Adds in a method that will select the desired default resolution button
    if it is present on the video.
    Just need to add in a user configurable setting for the default resolution
    that they would like to have loaded.

--- utils.js
+++ utils.js
@@ -14,3 +14,19 @@ function saveSettings() {
     chrome.storage.sync.set({"settings": settings});
     saveSettingsTimeout = setTimeout(saveSettings, 10000); // reset countdown
 }
+
+// TODO - add user configurable way to select the default resolution
+var defaultResolution = "720p";
+function setDefaultResolution(){
+    console.log("Called set resolution");
+    resolutionButtons = document.getElementsByClassName("explicit-resolution");
+    console.log("Should have the buttons?");
+    console.log(resolutionButtons)
+    for(const resButton of resolutionButtons){
+        if(resButton.firstChild.innerText == defaultResolution){
+            resButton.click();
+            console.log("Should have set default resolution to: " + defaultResolution);
+            break;
+        }
+    }
+}
